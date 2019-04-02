5fb77627237c0edeb4507f54962233ed7c3c25e9
gaoyj <gaoyijun19920213@163.com>
2019-04-02 15:24:36 +0800

    ReactDOM测试

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends Component {
+    render () {
+      return (
+        <div>
+          <h1>React 小书</h1>
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
